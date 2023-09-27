044d52527a9578a91f18c0f47790984e7b360bd8
Azim Chowdhury <azimchy994@gmail.com>
2023-09-27 14:44:09 +0600

    dynamic side bar

--- src/app/(withLayout)/layout.tsx
+++ src/app/(withLayout)/layout.tsx
@@ -1,10 +1,11 @@
+import Contents from '@/components/ui/contents';
 import Sidebar from '@/components/ui/sidebar';
 import { Layout } from 'antd'
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 
     return <Layout hasSider>
         <Sidebar />
-        {children}
+        <Contents>{children}</Contents>
     </Layout>
 };
 
