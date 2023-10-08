46034982989e8b92184ff8f85d88624fc9299f9d
Azim Chowdhury <azimchy994@gmail.com>
2023-10-08 17:44:57 +0600

    academic faculty

---
+++ src/app/(withLayout)/admin/manage-faculty/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageFaculty = () => {
+    return (
+        <div>
+            <h1>this is manage faculty page</h1>
+        </div>
+    );
+};
+
+export default ManageFaculty;
